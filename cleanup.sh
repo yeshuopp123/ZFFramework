b790e095be1186dc43957f9edfdf51a930b44a24
ZSaberLv0 <z@zsaber.com>
2017-01-22 11:07:42 +0800

    update build script

--- cleanup.sh
+++ cleanup.sh
@@ -7,11 +7,11 @@ PROJ_ROOT=$WORK_DIR
 CLEAN_WHAT=$1
 
 if test "x-$CLEAN_WHAT" = "x-" || test "x-$CLEAN_WHAT" = "x-build" ; then
-    sh "$ZF_TOOLS_PATH/common/zfsh_recursive.sh" "$PROJ_ROOT" "zfautoscript_cleanup*.zfsh" >/dev/null 2>&1
+    sh "$ZF_TOOLS_PATH/common/zfsh_recursive.sh" "$PROJ_ROOT" "zfautoscript_cleanup*.zfsh"
 fi
 
 if test "x-$CLEAN_WHAT" = "x-" || test "x-$CLEAN_WHAT" = "x-zf3rd" ; then
-    sh "$ZF_TOOLS_PATH/common/zfsh_recursive.sh" "$PROJ_ROOT" "zfautoscript_zf3rd_cleanup*.zfsh" >/dev/null 2>&1
+    sh "$ZF_TOOLS_PATH/common/zfsh_recursive.sh" "$PROJ_ROOT" "zfautoscript_zf3rd_cleanup*.zfsh"
 fi
 
 rm -rf "$PROJ_ROOT/_release" >/dev/null 2>&1
