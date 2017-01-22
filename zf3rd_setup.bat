f639eb9202cb56e911b68bedc58c56295fc2d0f0
ZSaberLv0 <z@zsaber.com>
2017-01-22 11:17:05 +0800

    update build script

--- zf3rd_setup.bat
+++ zf3rd_setup.bat
@@ -5,5 +5,5 @@ set WORK_DIR=%~dp0
 set ZF_ROOT_PATH=%WORK_DIR%
 set ZF_TOOLS_PATH=%ZF_ROOT_PATH%\tools
 
-call "%ZF_TOOLS_PATH%\common\zfsh_recursive.bat" "%ZF_ROOT_PATH%" "zfautoscript_zf3rd*.zfsh"
+call "%ZF_TOOLS_PATH%\common\zfsh_recursive.bat" "%ZF_ROOT_PATH%" "zfautoscript_zf3rd_setup*.zfsh"
 
--- zf3rd_setup.sh
+++ zf3rd_setup.sh
@@ -4,5 +4,5 @@ WORK_DIR=$(cd "$(dirname "$0")"; pwd)
 ZF_ROOT_PATH=$WORK_DIR
 ZF_TOOLS_PATH=$ZF_ROOT_PATH/tools
 
-sh "$ZF_TOOLS_PATH/common/zfsh_recursive.sh" "$ZF_ROOT_PATH" "zfautoscript_zf3rd*.zfsh"
+sh "$ZF_TOOLS_PATH/common/zfsh_recursive.sh" "$ZF_ROOT_PATH" "zfautoscript_zf3rd_setup*.zfsh"
 
